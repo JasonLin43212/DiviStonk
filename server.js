efa805d9a224e4521a4e1431a257413edca0e995
Jason Lin <jasonlin43212@gmail.com>
2020-06-22 23:59:25 -0400

    heroku test 2

--- server.js
+++ server.js
@@ -15,3 +15,9 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+// Select port
+const port = process.env.PORT || 5000;
+
+// Make app listen to that port
+app.listen(port, () => console.log(`Server started on port ${port}`));
