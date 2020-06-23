5cbade6f439ee2bd07a02dfae65245c4f6d8bb63
Jason Lin <jasonlin43212@gmail.com>
2020-06-22 23:48:13 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    // Load index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
